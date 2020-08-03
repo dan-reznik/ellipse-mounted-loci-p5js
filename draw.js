82dd359c4e25fe41333844f5b972cf33527a3821
Dan Reznik <dreznik@gmail.com>
2020-08-03 16:15:47 -0300

    played w dashed

--- draw.js
+++ draw.js
@@ -240,35 +240,16 @@ function draw_mounted(ons, clr, dr_sidelengths = true, dr_dashed=false) {
   pop();
 }
 
-function linedash_old(x1, y1, x2, y2, delta, style = '-') {
-  // delta is both the length of a dash, the distance between 2 dots/dashes, and the diameter of a round
-  let distance = dist(x1,y1,x2,y2);
-  let dashNumber = distance/delta;
-  let xDelta = (x2-x1)/dashNumber;
-  let yDelta = (y2-y1)/dashNumber;
-
-  for (let i = 0; i < dashNumber; i+= 2) {
-    let xi1 = i*xDelta + x1;
-    let yi1 = i*yDelta + y1;
-    let xi2 = (i+1)*xDelta + x1;
-    let yi2 = (i+1)*yDelta + y1;
-
-    if (style == '-') { line(xi1, yi1, xi2, yi2); }
-    else if (style == '.') { point(xi1, yi1); }
-    else if (style == 'o') { ellipse(xi1, yi1, delta/2); }
-  }
-}
-
-
-function linedash(p1, p2, drho) {
+function linedash(p1, p2, dd) {
+  let d12 = edist(p1,p2);
+  let phat = vnorm(vdiff(p2,p1));
   let flag = true;
-  for (let rho = 0; rho < 1-drho; rho += drho) {
+  for (let d = 0; d < d12-dd; d += dd) {
     if (flag) {
-      let from = vinterp(p1, p2, rho);
-      let to = vinterp(p1, p2, rho + drho);
+      let from = vsum(p1,vscale(phat,d));
+      let to = vsum(from,vscale(phat,dd));
       line(from[0], from[1], to[0], to[1]);
-    } else
-       rho -= 0.5*drho;
+    } else d += dd;
     flag = !flag;
   }
 }
@@ -296,9 +277,9 @@ function draw_tri_dashed([p1, p2, p3], rgb, wgt = 0.015) {
   noFill();
   stroke(rgb);
   strokeWeight(wgt);
-  linedash(p1, p2, 0.05);
-  linedash(p2, p3, 0.05);
-  linedash(p3, p1, 0.05);
+  linedash(p1, p2, 0.025);
+  linedash(p2, p3, 0.025);
+  linedash(p3, p1, 0.025);
   pop();
 }
 
