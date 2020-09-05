e7b773ad8e4a24fc6e846133cc301ee0545adb1d
Dan Reznik <dreznik@gmail.com>
2020-09-05 19:17:34 -0300

    simple polygon

--- simple-polygon.js
+++ simple-polygon.js
@@ -1,6 +1,9 @@
 var simplepolygon = require('simplepolygon')
 // what I will export
 module.exports = {simplepolygon:simplepolygon};
+
+// browserify .\simple-polygon.js --standalone bundle > .\simple-polygon-b.js
+
 /* var poly = {
 "type": "Feature",
  "geometry": {
@@ -8,6 +11,9 @@ module.exports = {simplepolygon:simplepolygon};
    "coordinates": [[[0,0],[2,0],[0,2],[2,2],[0,0]]]
  }
 };
-var result = simplepolygon(poly);
-console.log(result); */
-// browserify .\simple-polygon.js --standalone bundle > .\simple-polygon-b.js
+var subpolys = simplepolygon(poly);
+console.log(result);
+*/
+
+// subpolys = bundle.simplepolygon(poly)
+//subpolys.features.map(f=>f.geometry.coordinates[0])
