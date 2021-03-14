8e5b09ea8dcf53cc16e18b7c6ff2619823c63093
Dan Reznik <dreznik@gmail.com>
2021-03-14 11:39:28 -0300

    3 lines on output

--- invariants.js
+++ invariants.js
@@ -8,15 +8,16 @@ function get_LArR_low(sides) {
     const l2inv = sum(sides.map(s=>1/(s*s)));
     const cotw =  l2/(4*A);
     const cosprod = product(tri_cosines(sides));
-    return {L:L,A:A,r:r,R:R,rR:safe_div(r,R),cotw:cotw,l2:l2,linv:linv,l2inv:l2inv,cosprod:cosprod};
+    const lprod = product(sides);
+    return {L:L,A:A,r:r,R:R,rR:safe_div(r,R),cotw:cotw,l2:l2,linv:linv,l2inv:l2inv,cosprod:cosprod,lprod:lprod};
  }
  
  function get_info_arr(info,info_der,not_ref) {
     const basics =  [info.L, info.A, info.r, info.R, info.rR,
-       info.cotw, info.l2, info.linv, info.l2inv, info.cosprod];
+       info.cotw, info.l2, info.linv, info.l2inv, info.cosprod,info.lprod];
     return not_ref?
     basics.concat([info_der.L, info_der.A, info_der.r, info_der.R, info_der.rR,
-       info_der.cotw, info_der.l2, info_der.linv, info_der.l2inv, info_der.cosprod,
+       info_der.cotw, info_der.l2, info_der.linv, info_der.l2inv, info_der.cosprod,info_der.lprod,
     // ["L'/L", "A'/A", "A'.A"]
     safe_div(info_der.L, info.L), safe_div(info_der.A, info.A), info_der.A*info.A]):
     basics;
@@ -29,7 +30,7 @@ function get_LArR_low(sides) {
     return {info:info, info_der:info_der, vals:vals};
  }
  
- //  ["L", "A", "r", "R", "r/R", "cot(ω)", "Σs^2","Σ(1/s)","1/Σs^-2","∏cos"
+ //  ["L", "A", "r", "R", "r/R", "cot(ω)", "Σs^2","Σ(1/s)","Σs^-2","∏cos","∏s"
  
  function get_orbit_biz(a, tDeg, mounting, tri_type, cpn, pn, circ, inv, not_ref) {
     const orbit = mounting in dict_orbit_fn ?
@@ -45,11 +46,17 @@ function get_LArR_low(sides) {
     const biz2 = get_orbit_biz(a, tDeg+5., mounting, tri_type, cpn, pn, circ, inv, not_ref);
     const biz3 = get_orbit_biz(a, tDeg-10., mounting, tri_type, cpn, pn, circ, inv, not_ref);
     
-    const labs0 = ["L", "A", "r", "R", "r/R", "cot(ω)", "Σs^2","Σ(1/s)","Σs^-2","∏cos"];
+    const labs0 = ["L", "A", "r", "R", "r/R", "cot(ω)", "Σs^2","Σ(1/s)","Σs^-2","∏cos","∏s"];
     const labs = not_ref ? labs0.concat(labs0.map(l=>l+"'")).concat(["L'/L", "A'/A", "A'.A"]): labs0;
     let str_invs = [];
-    labs.map((l,i)=> {if(same_triple([biz1,biz2,biz3].map(b=>b.info.vals[i])))
-       str_invs.push(sprintf(l+"=%.5f",biz1.info.vals[i]));});
-    return { o: biz1.derived.o, s: biz1.derived.s, str: str_invs.join(", "), lines: not_ref ? /*3*/1 : 1 };
+     labs.map((l, i) => {
+         if ([labs0.length, 2 * labs0.length].includes(i))
+             str_invs.push("\n");
+         if (same_triple([biz1, biz2, biz3].map(b => b.info.vals[i])))
+             str_invs.push(sprintf(l + "=%.5f", biz1.info.vals[i]));
+     });
+    const str_join = str_invs.join(", ");
+    const str_repl = str_join.replace(/, \n, /g,"\n").replace(/, \n/g,"");
+    return { o: biz1.derived.o, s: biz1.derived.s, str: str_repl, lines: not_ref ? 3 : 1 };
  }
  
